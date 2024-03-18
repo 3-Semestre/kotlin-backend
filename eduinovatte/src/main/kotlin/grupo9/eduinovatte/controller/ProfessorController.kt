4bf713862c58be96f55cd1be4f36da2080c989e3
GabrielBoos22 <gabriel.bduarte@sptech.school>
2024-03-17 22:37:57 -0300

    Alterando lógica de verificar professor

--- eduinovatte/src/main/kotlin/grupo9/eduinovatte/controller/ProfessorController.kt
+++ eduinovatte/src/main/kotlin/grupo9/eduinovatte/controller/ProfessorController.kt
@@ -1,6 +1,6 @@
 package grupo9.eduinovatte.controller
 
-import grupo9.eduinovatte.model.Professor
+import grupo9.eduinovatte.model.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,10 +11,26 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
 @RequestMapping("/professor")
 class ProfessorController {
-    var ListaProfessores: MutableList<Professor> = mutableListOf()
+    var ListaProfessores: MutableList<Professor> = mutableListOf(Professor(
+        nomeCompleto = "Cristian",
+        email = "cristian@email.com",
+        senha = "banana123",
+        endereco = Endereco(
+            logradouro = "Rua Dez",
+            complemento = 10,
+            bairro ="Vila Redenção",
+            localidade = "Imperatriz",
+            uf = "MA"
+            ),
+        experiencia = Experiencia(
+            nivel = Nivel.A1,
+            escolaridade = Escolaridade.PosGraduacao
+        )
+    ))
 
     @GetMapping
     fun buscaProfessores(): ResponseEntity<List<Professor>>{
@@ -39,15 +55,16 @@ class ProfessorController {
         return ResponseEntity.status(404).build()
     }
 
-    @DeleteMapping("/{id}")
-    fun deletaProfessor(@PathVariable id: Int): ResponseEntity<Void> {
-        if(existeProfessor(id)){
-            ListaProfessores.removeAt(id)
+    @DeleteMapping("/{indice}")
+    fun deletaProfessor(@PathVariable indice: Int): ResponseEntity<Void> {
+        if(existeProfessor(indice)){
+            ListaProfessores.removeAt(indice - 1)
+            return ResponseEntity.status(204).build()
         }
         return ResponseEntity.status(404).build()
     }
 
-    fun existeProfessor(id: Int): Boolean{
-        return id >= 0 && id <= ListaProfessores.size
+    fun existeProfessor(indice: Int): Boolean{
+        return indice > 0 && indice <= ListaProfessores.size
     }
 }

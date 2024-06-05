56072f2d75bc27c25dd4473295d3803a749eac4d
xGustav1n <gustavo.pereira@sptech.school>
2024-06-05 09:34:31 -0300

    Adicionando operation e Apiresponse nas classe UsuarioNicho

--- eduinovatte/src/main/kotlin/grupo9/eduinovatte/application/controller/UsuarioNichoController.kt
+++ eduinovatte/src/main/kotlin/grupo9/eduinovatte/application/controller/UsuarioNichoController.kt
@@ -4,6 +4,9 @@ import grupo9.eduinovatte.model.UsuarioNicho
 import grupo9.eduinovatte.model.enums.NichoNome
 import grupo9.eduinovatte.model.enums.NivelInglesNome
 import grupo9.eduinovatte.service.UsuarioNichoRepository
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,6 +20,11 @@ class UsuarioNichoController(
     val usuarioNichoRepository: UsuarioNichoRepository
 ) {
 
+    @Operation(summary = "Busque os usuario com o filtro", description = "Busque todos os usuarios com filtro nicho dos professores e alunos.")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Usuarios encontrados encontrados"),
+        ApiResponse(responseCode = "204", description = "Nenhum Usuarios encontrado")
+    ])
     @GetMapping("/nicho/{nichoNome}")
     fun buscarUsuarioNicho(@PathVariable nichoNome: String): ResponseEntity<List<UsuarioNicho>> {
         val nichoEnum = try {
@@ -33,6 +41,11 @@ class UsuarioNichoController(
         }
     }
 
+    @Operation(summary = "Busque os usuario com o filtro", description = "Busque todos os usuarios com filtro nivel de ingles dos professores e alunos.")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Usuarios encontrados encontrados"),
+        ApiResponse(responseCode = "204", description = "Nenhum Usuarios encontrado")
+    ])
     @GetMapping("/nivel-ingles/{nivelInglesNome}")
     fun buscarUsuarioNivelIngles(@PathVariable nivelInglesNome: String): ResponseEntity<List<UsuarioNicho>> {
         val inglesEnum = try {

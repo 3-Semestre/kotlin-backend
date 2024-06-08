f820de9905c75ba421a9c09b2060060f48a0c852
Miguel Silva <gisele.silva@sptech.school>
2024-06-08 20:40:51 -0300

    dash?

--- eduinovatte/src/main/kotlin/grupo9/eduinovatte/application/EduinovatteApplication.kt
+++ eduinovatte/src/main/kotlin/grupo9/eduinovatte/application/EduinovatteApplication.kt
@@ -2,8 +2,10 @@ package grupo9.eduinovatte
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class EduinovatteApplication
 
 fun main(args: Array<String>) {
